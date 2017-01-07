d3f874c46898ebe313a532c18cb39a2804102a9b
Brian Barrow <briancbarrow@gmail.com>
2017-01-06 21:18:01 -0700

    learned about pipes, directives, modules etc.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { BoldHoverDirective } from './directives/bold-hover.directive'
+import { UnlessDirective } from './directives/unless.directive'
+import { CapitalizePipe } from './pipes/capitalize.pipe'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // pipes: [CapitalizePipe]
+  // directives: [BoldHoverDirective]
 })
 export class AppComponent {
   constructor() {} //Why did I need to put this here in order for the type declaration to work?
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+// import { FormsModule } from '@angular/forms';
+// import { HttpModule } from '@angular/http';
+import { SharedModule } from './shared/shared.module'
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component'
+import { HeaderComponent } from './shared/header/header.component';
+import { BoldHoverDirective } from './directives/bold-hover.directive';
+import { UnlessDirective } from './directives/unless.directive';
+import { CapitalizePipe } from './pipes/capitalize.pipe'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BoldHoverDirective,
+    UnlessDirective,
+    CapitalizePipe
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    SharedModule
+    // FormsModule,
+    // HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
